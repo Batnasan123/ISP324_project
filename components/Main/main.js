59b921accd7c42556c59313538737dbcae77d652
Batnasan <b20fa1703@ufe.edu.mn>
2023-10-23 23:31:23 +0800

    update

--- components/Main/main.js
+++ components/Main/main.js
@@ -1,5 +1,12 @@
 import Image from "next/image";
+import Features from "../features/index";
+import Banner from "../banner/index";
 
 export default function Main() {
-  return <div className={`min-h-screen`}>Main page</div>;
+  return (
+    <div className="">
+      <Banner />
+      <Features />
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 import Image from "next/image";
 import Main from "../components/Main/main";
-import Footer from "../components/footer";
-import TopBar from "../components/topbar";
 
 export default function Home() {
   return (
