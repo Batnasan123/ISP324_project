684cd1fef22f83bed0b5ebff55a8ffc52ddc2d24
Sumiyabazar <99630109+Sumiyabazarr@users.noreply.github.com>
2023-11-05 17:01:02 +0800

    Add files via upload

---
+++ components/about/About.jsx
@@ -0,0 +1,200 @@
+import React from "react";
+import "./style.css";
+
+export const About = () => {
+  return (
+    <div className="about">
+      <div className="overlap-wrapper">
+        <div className="overlap">
+          <img className="footer" alt="Footer" src="/img/footer.png" />
+          <div className="testimonials">
+            <div className="content">
+              <div className="title">
+                <div className="text-wrapper">TESTIMONIALS</div>
+                <div className="div">What our Customers says...</div>
+              </div>
+              <div className="testimonial">
+                <div className="element">
+                  <div className="div-wrapper">
+                    <div className="text">
+                      <p className="p">
+                        “ It is a long established fact that a reader will be tracked distracted by the readable content
+                        of a page is when looking at its layout. The point of using Lorem of distribution it look like
+                        readable English “
+                      </p>
+                    </div>
+                  </div>
+                  <div className="user">
+                    <div className="user-2">
+                      <div className="user-3">
+                        <img className="image" alt="Image" src="/img/image.png" />
+                        <div className="name">
+                          <img className="reviews" alt="Reviews" src="/img/reviews-1.svg" />
+                          <div className="text-wrapper-2">James Williams</div>
+                          <div className="text-wrapper-3">United States</div>
+                        </div>
+                      </div>
+                      <img className="icon" alt="Icon" src="/img/icon-3.png" />
+                    </div>
+                  </div>
+                </div>
+                <div className="element">
+                  <div className="div-wrapper">
+                    <div className="text">
+                      <p className="p">
+                        “ It is a long established fact that a reader will be tracked distracted by the readable content
+                        of a page is when looking at its layout. The point of using Lorem of distribution it look like
+                        readable English “
+                      </p>
+                    </div>
+                  </div>
+                  <div className="user">
+                    <div className="user-2">
+                      <div className="user-3">
+                        <img className="image" alt="Image" src="/img/image-4.png" />
+                        <div className="name">
+                          <img className="reviews" alt="Reviews" src="/img/reviews.svg" />
+                          <div className="text-wrapper-2">Lieo Jessica</div>
+                          <div className="text-wrapper-3">United Kingdom</div>
+                        </div>
+                      </div>
+                      <img className="icon" alt="Icon" src="/img/icon-3.png" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="overlap-group">
+            <div className="our-team">
+              <div className="overlap-group-2">
+                <div className="background" />
+                <div className="our-team-2">
+                  <div className="title-2">
+                    <div className="text-wrapper">MEET OUR EXPERTS</div>
+                    <p className="text-wrapper-4">The amazing women behind Beautyness</p>
+                  </div>
+                  <div className="content-2">
+                    <div className="element-2">
+                      <img className="img" alt="Image" src="/img/image-3.png" />
+                      <div className="content-3">
+                        <div className="name-2">
+                          <div className="text-wrapper-5">Sherlin D’Cruz</div>
+                          <div className="beautyness-expert">BEAUTYNESS EXPERT</div>
+                        </div>
+                        <div className="content-4">
+                          <p className="text-wrapper-6">
+                            I must explain to you how all this mistaken idea of denouncing pleasure that will araise
+                            praising pain
+                          </p>
+                          <div className="icon-2">
+                            <div className="text-wrapper-7"></div>
+                            <div className="text-wrapper-7"></div>
+                            <div className="text-wrapper-7"></div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="element-2">
+                      <img className="img" alt="Image" src="/img/image-2.png" />
+                      <div className="content-3">
+                        <div className="name-2">
+                          <div className="text-wrapper-5">Maria Sharapova</div>
+                          <div className="beautyness-expert">BEAUTYNESS EXPERT</div>
+                        </div>
+                        <div className="content-4">
+                          <p className="text-wrapper-6">
+                            I must explain to you how all this mistaken idea of denouncing pleasure that will araise
+                            praising pain
+                          </p>
+                          <div className="icon-2">
+                            <div className="text-wrapper-7"></div>
+                            <div className="text-wrapper-7"></div>
+                            <div className="text-wrapper-7"></div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="element-2">
+                      <img className="img" alt="Image" src="/img/image-1.png" />
+                      <div className="content-3">
+                        <div className="name-2">
+                          <div className="text-wrapper-5">Angeline Jenifer</div>
+                          <div className="beautyness-expert">BEAUTYNESS EXPERT</div>
+                        </div>
+                        <div className="content-4">
+                          <p className="text-wrapper-6">
+                            I must explain to you how all this mistaken idea of denouncing pleasure that will araise
+                            praising pain
+                          </p>
+                          <div className="icon-2">
+                            <div className="text-wrapper-7"></div>
+                            <div className="text-wrapper-7"></div>
+                            <div className="text-wrapper-7"></div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <img className="img-2" alt="About" src="/img/about.png" />
+            <div className="features">
+              <div className="overlap-2">
+                <div className="background-2" />
+                <div className="content-5">
+                  <div className="title-2">
+                    <div className="text-wrapper">OUR VALUES</div>
+                    <p className="text-wrapper-4">The work values we thrive for</p>
+                  </div>
+                  <div className="content-6">
+                    <div className="element-3">
+                      <div className="icon-wrapper">
+                        <img className="icon-3" alt="Icon" src="/img/icon-2.png" />
+                      </div>
+                      <div className="content-7">
+                        <div className="text-wrapper-8">Beauty Experts</div>
+                        <p className="text-wrapper-9">
+                          The majority have suffered alteration in some form, buying to injected humour, or randomised
+                          words which desktop publishing packages.
+                        </p>
+                      </div>
+                    </div>
+                    <img className="line" alt="Line" src="/img/line.png" />
+                    <div className="element-3">
+                      <div className="icon-wrapper">
+                        <img className="icon-4" alt="Icon" src="/img/icon-1.png" />
+                      </div>
+                      <div className="content-7">
+                        <div className="text-wrapper-8">Great Services</div>
+                        <p className="text-wrapper-10">
+                          The majority have suffered alteration in some form, buying to injected humour, or randomised
+                          words which desktop publishing packages.
+                        </p>
+                      </div>
+                    </div>
+                    <img className="line" alt="Line" src="/img/line.png" />
+                    <div className="element-3">
+                      <div className="icon-wrapper">
+                        <img className="icon-5" alt="Icon" src="/img/icon.png" />
+                      </div>
+                      <div className="content-7">
+                        <div className="text-wrapper-8">100% Genuine</div>
+                        <p className="text-wrapper-9">
+                          The majority have suffered alteration in some form, buying to injected humour, or randomised
+                          words which desktop publishing packages.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <img className="banner" alt="Banner" src="/img/banner.png" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
