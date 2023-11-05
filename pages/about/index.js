26a91c3a80eaee12aed49906cea5d3012a2a2e7b
Batnasan <b20fa1703@ufe.edu.mn>
2023-11-05 18:37:09 +0800

    css fixed

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,7 +1,7 @@
 import React from "react";
-import "./styles/about.css";
+// import "./styles/about.css";
 
-export const About = () => {
+export default function About() {
   return (
     <div className="about">
       <div className="overlap-wrapper">
@@ -18,18 +18,27 @@ export const About = () => {
                   <div className="div-wrapper">
                     <div className="text">
                       <p className="p">
-                        “ It is a long established fact that a reader will be tracked distracted by the readable content
-                        of a page is when looking at its layout. The point of using Lorem of distribution it look like
-                        readable English “
+                        “ It is a long established fact that a reader will be
+                        tracked distracted by the readable content of a page is
+                        when looking at its layout. The point of using Lorem of
+                        distribution it look like readable English “
                       </p>
                     </div>
                   </div>
                   <div className="user">
                     <div className="user-2">
                       <div className="user-3">
-                        <img className="image" alt="Image" src="/img/image.png" />
+                        <img
+                          className="image"
+                          alt="Image"
+                          src="/img/image.png"
+                        />
                         <div className="name">
-                          <img className="reviews" alt="Reviews" src="/img/reviews-1.svg" />
+                          <img
+                            className="reviews"
+                            alt="Reviews"
+                            src="/img/reviews-1.svg"
+                          />
                           <div className="text-wrapper-2">James Williams</div>
                           <div className="text-wrapper-3">United States</div>
                         </div>
@@ -42,18 +51,27 @@ export const About = () => {
                   <div className="div-wrapper">
                     <div className="text">
                       <p className="p">
-                        “ It is a long established fact that a reader will be tracked distracted by the readable content
-                        of a page is when looking at its layout. The point of using Lorem of distribution it look like
-                        readable English “
+                        “ It is a long established fact that a reader will be
+                        tracked distracted by the readable content of a page is
+                        when looking at its layout. The point of using Lorem of
+                        distribution it look like readable English “
                       </p>
                     </div>
                   </div>
                   <div className="user">
                     <div className="user-2">
                       <div className="user-3">
-                        <img className="image" alt="Image" src="/img/image-4.png" />
+                        <img
+                          className="image"
+                          alt="Image"
+                          src="/img/image-4.png"
+                        />
                         <div className="name">
-                          <img className="reviews" alt="Reviews" src="/img/reviews.svg" />
+                          <img
+                            className="reviews"
+                            alt="Reviews"
+                            src="/img/reviews.svg"
+                          />
                           <div className="text-wrapper-2">Lieo Jessica</div>
                           <div className="text-wrapper-3">United Kingdom</div>
                         </div>
@@ -72,7 +90,9 @@ export const About = () => {
                 <div className="our-team-2">
                   <div className="title-2">
                     <div className="text-wrapper">MEET OUR EXPERTS</div>
-                    <p className="text-wrapper-4">The amazing women behind Beautyness</p>
+                    <p className="text-wrapper-4">
+                      The amazing women behind Beautyness
+                    </p>
                   </div>
                   <div className="content-2">
                     <div className="element-2">
@@ -80,12 +100,14 @@ export const About = () => {
                       <div className="content-3">
                         <div className="name-2">
                           <div className="text-wrapper-5">Sherlin D’Cruz</div>
-                          <div className="beautyness-expert">BEAUTYNESS EXPERT</div>
+                          <div className="beautyness-expert">
+                            BEAUTYNESS EXPERT
+                          </div>
                         </div>
                         <div className="content-4">
                           <p className="text-wrapper-6">
-                            I must explain to you how all this mistaken idea of denouncing pleasure that will araise
-                            praising pain
+                            I must explain to you how all this mistaken idea of
+                            denouncing pleasure that will araise praising pain
                           </p>
                           <div className="icon-2">
                             <div className="text-wrapper-7"></div>
@@ -100,12 +122,14 @@ export const About = () => {
                       <div className="content-3">
                         <div className="name-2">
                           <div className="text-wrapper-5">Maria Sharapova</div>
-                          <div className="beautyness-expert">BEAUTYNESS EXPERT</div>
+                          <div className="beautyness-expert">
+                            BEAUTYNESS EXPERT
+                          </div>
                         </div>
                         <div className="content-4">
                           <p className="text-wrapper-6">
-                            I must explain to you how all this mistaken idea of denouncing pleasure that will araise
-                            praising pain
+                            I must explain to you how all this mistaken idea of
+                            denouncing pleasure that will araise praising pain
                           </p>
                           <div className="icon-2">
                             <div className="text-wrapper-7"></div>
@@ -120,12 +144,14 @@ export const About = () => {
                       <div className="content-3">
                         <div className="name-2">
                           <div className="text-wrapper-5">Angeline Jenifer</div>
-                          <div className="beautyness-expert">BEAUTYNESS EXPERT</div>
+                          <div className="beautyness-expert">
+                            BEAUTYNESS EXPERT
+                          </div>
                         </div>
                         <div className="content-4">
                           <p className="text-wrapper-6">
-                            I must explain to you how all this mistaken idea of denouncing pleasure that will araise
-                            praising pain
+                            I must explain to you how all this mistaken idea of
+                            denouncing pleasure that will araise praising pain
                           </p>
                           <div className="icon-2">
                             <div className="text-wrapper-7"></div>
@@ -146,44 +172,61 @@ export const About = () => {
                 <div className="content-5">
                   <div className="title-2">
                     <div className="text-wrapper">OUR VALUES</div>
-                    <p className="text-wrapper-4">The work values we thrive for</p>
+                    <p className="text-wrapper-4">
+                      The work values we thrive for
+                    </p>
                   </div>
                   <div className="content-6">
                     <div className="element-3">
                       <div className="icon-wrapper">
-                        <img className="icon-3" alt="Icon" src="/img/icon-2.png" />
+                        <img
+                          className="icon-3"
+                          alt="Icon"
+                          src="/img/icon-2.png"
+                        />
                       </div>
                       <div className="content-7">
                         <div className="text-wrapper-8">Beauty Experts</div>
                         <p className="text-wrapper-9">
-                          The majority have suffered alteration in some form, buying to injected humour, or randomised
-                          words which desktop publishing packages.
+                          The majority have suffered alteration in some form,
+                          buying to injected humour, or randomised words which
+                          desktop publishing packages.
                         </p>
                       </div>
                     </div>
                     <img className="line" alt="Line" src="/img/line.png" />
                     <div className="element-3">
                       <div className="icon-wrapper">
-                        <img className="icon-4" alt="Icon" src="/img/icon-1.png" />
+                        <img
+                          className="icon-4"
+                          alt="Icon"
+                          src="/img/icon-1.png"
+                        />
                       </div>
                       <div className="content-7">
                         <div className="text-wrapper-8">Great Services</div>
                         <p className="text-wrapper-10">
-                          The majority have suffered alteration in some form, buying to injected humour, or randomised
-                          words which desktop publishing packages.
+                          The majority have suffered alteration in some form,
+                          buying to injected humour, or randomised words which
+                          desktop publishing packages.
                         </p>
                       </div>
                     </div>
                     <img className="line" alt="Line" src="/img/line.png" />
                     <div className="element-3">
                       <div className="icon-wrapper">
-                        <img className="icon-5" alt="Icon" src="/img/icon.png" />
+                        <img
+                          className="icon-5"
+                          alt="Icon"
+                          src="/img/icon.png"
+                        />
                       </div>
                       <div className="content-7">
                         <div className="text-wrapper-8">100% Genuine</div>
                         <p className="text-wrapper-9">
-                          The majority have suffered alteration in some form, buying to injected humour, or randomised
-                          words which desktop publishing packages.
+                          The majority have suffered alteration in some form,
+                          buying to injected humour, or randomised words which
+                          desktop publishing packages.
                         </p>
                       </div>
                     </div>
@@ -197,4 +240,4 @@ export const About = () => {
       </div>
     </div>
   );
-};
+}
