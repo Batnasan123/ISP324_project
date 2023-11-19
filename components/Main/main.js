82fb0549ace4f245b69f318da1bb3a8f17f7496a
Batnasan <b20fa1703@ufe.edu.mn>
2023-11-19 22:54:02 +0800

    update

--- components/Main/main.js
+++ components/Main/main.js
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import Features from "../features/index";
 import Banner from "../banner/index";
+import Aboutus from "../landing/aboutus";
+import Testimonials from "../landing/testimonials";
 
 export default function Main() {
   return (
     <div className="">
       <Banner />
       <Features />
+      <Aboutus />
+      <Testimonials />
     </div>
   );
 }
